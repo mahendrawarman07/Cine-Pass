f779d65d0f9b756a5436ed7ea7fac5d9b85e356a
Mahendrawarman <mahendrawarmank@gmail.com>
2025-11-15 17:40:11 +0530

    Movie api has created

---
+++ project/server/routes/movie.route.js
@@ -0,0 +1,126 @@
+const express = require('express')
+const Movie = require('../models/movie.model.js')
+
+
+const movieRouter = express.Router(); // Route
+
+
+// Add a Movie
+
+movieRouter.post('/add-movie' , async(req , res)=>{
+   try {
+     const newMovie = new Movie(req.body)
+     await newMovie.save()
+       res.send({
+            success: true,
+            message: 'New movie has been added!'
+        })
+   } catch (error) {
+         res.send({
+            success: false,
+            message: 'Movie Could not be added'
+        })
+   }
+})
+
+
+
+// update movie
+
+movieRouter.put('/update-movie/:id' , async(req , res)=>{
+    try {
+     const movieId = req.params.id
+     const movie = await Movie.findByIdAndUpdate(movieId , req.body,{ new: true, runValidators: true }) // important options
+       res.send({
+            success: true,
+            message: 'The movie has been updated!',
+            data: movie
+        })
+    } catch (error) {
+           res.send({
+            success: false,
+            message: 'Server Error'
+        })
+    }
+
+})
+
+
+
+
+// Delete Movie
+
+movieRouter.delete('/delete-movie/:id', async (req, res) => {
+  try {
+    const movieId = req.params.id;
+
+    const movie = await Movie.findByIdAndDelete(movieId);
+
+    if (!movie) {
+      return res.status(404).send({
+        success: false,
+        message: 'Movie not found',
+      });
+    }
+
+    res.send({
+      success: true,
+      message: 'The movie has been deleted!',
+      data: movie
+    });
+
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: 'Server Error',
+      error: error.message
+    });
+  }
+});
+
+
+
+// get all Movies
+movieRouter.get('/all-movies' , async(req , res)=>{
+      try {
+         const allMovies =  await Movie.find()
+         res.send({
+            success: true,
+            message: 'All movies have been fetched!',
+            data: allMovies
+        });
+
+      } catch (error) {
+          res.send({
+            success: false,
+            message: error.message
+        });
+      }
+})
+
+// get a specific Movie
+
+movieRouter.get('/:id' , async(req , res)=>{
+    try {
+        const movie = await Movie.findById(req.params.id)
+         res.send({
+            success: true,
+            message: "Movie fetched successfully!",
+            data: movie
+        })
+    } catch (error) {
+           res.send({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+
+
+
+
+module.exports =movieRouter;
+
+
+
